478ce10f66b7f796afbcacfc71d1165806d76023
mateo <desarrollo11@aveonline.co>
2023-08-02 16:47:03 -0500

    fix: delete unnecesary hook

--- pages/gestion-clientes/index.tsx
+++ pages/gestion-clientes/index.tsx
@@ -1,7 +1,7 @@
 import { Layout, LayoutHydrated, Table } from "@/components";
 import { Person } from "@/types";
 import { useColumTable } from "@/hooks";
-import { ReactElement, useEffect } from "react";
+import { ReactElement } from "react";
 import { route } from "@/constants";
 import { useGetUsers } from "@/hooks/services/query/useGetUsers";
 import QueryResult from "@/components/QueryResult";
@@ -11,14 +11,11 @@ export default function CustomerManagement() {
 
   const { data, error, isLoading } = useGetUsers();
 
-    useEffect(() => {
-      console.log('data', data)
-    }, [data?.data])
-
+  const defaultData: Person[] = data ? data.data : []
   return (
     <div className=" flex justify-center items-center h-screen">
       <QueryResult error={error} isLoading={isLoading}>
-        <Table data={data?.data ?? []} columns={columns} />
+        <Table data={defaultData} columns={columns} />
       </QueryResult>
     </div>
   );
