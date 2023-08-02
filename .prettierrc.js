4e4a14229138667ab9ad5e2c1d2589d0558817a5
mateo <desarrollo11@aveonline.co>
2023-08-02 16:48:14 -0500

    fix: confit eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  trailingComma: 'none',
+  bracketSameLine: true,
+  endOfLine: 'lf',
+  jsxSingleQuote: true
+}
